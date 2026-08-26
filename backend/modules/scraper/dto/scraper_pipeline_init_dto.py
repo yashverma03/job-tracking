9b8a8c05149bd7957a62995595ed56f466395e8d
yashverma03 <vermayash2003@gmail.com>
2026-08-26 11:20:33 +0530

    feat: update default time_range_hours to 28 in scraper pipeline DTOs

--- backend/modules/scraper/dto/scraper_pipeline_init_dto.py
+++ backend/modules/scraper/dto/scraper_pipeline_init_dto.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class ScraperPipelineInitDTO(serializers.Serializer):
     max_jobs_per_run = serializers.IntegerField(min_value=1, max_value=1000, default=1000)
     start_offset = serializers.IntegerField(min_value=0, default=0)
-    time_range_hours = serializers.IntegerField(min_value=1, max_value=168, default=24)
+    time_range_hours = serializers.IntegerField(min_value=1, max_value=168, default=28)
