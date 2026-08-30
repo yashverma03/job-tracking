e9e759f2547803db48b91db88f63e493f4f09771
yashverma03 <vermayash2003@gmail.com>
2026-08-30 14:11:35 +0530

    feat: add 'Normal' option to CompanyType and update migration for company type field

--- backend/modules/company/enums/company_type.py
+++ backend/modules/company/enums/company_type.py
@@ -4,3 +4,4 @@ from django.db import models
 class CompanyType(models.TextChoices):
     TOP_COMPANY = 'topCompany', 'Top company'
     BLACKLIST = 'blacklist', 'Blacklist'
+    NORMAL = 'normal', 'Normal'
